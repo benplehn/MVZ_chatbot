c38d323ca7ee6b62197a1b36b6a8130cf9621a44
benplehn <benplehn@gmail.com>
2023-08-12 12:00:57 +0200

    Created models

--- chatbot_project/chatbot_app/models.py
+++ chatbot_project/chatbot_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class ChatMessage(models.Model):
+    USER = 'USER'
+    BOT = 'BOT'
+    SENDER_CHOICES = [
+        (USER, 'User'),
+        (BOT, 'Bot')
+    ]
+    
+    sender = models.CharField(max_length = 4, choices = SENDER_CHOICES, default = USER)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    session = models.ForeignKey('ChatSession', on_delete=models.CASCADE, related_name='message')
+
+
+class ChatSession(models.Model):
+    
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True, blank=True)
